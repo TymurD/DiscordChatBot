9eb58e9ef13c9c9bd688e1066d707768163f6461
TymurD <tymur2910@gmail.com>
2026-04-19 17:27:24 +0300

    fix script to have proper paths for creating and modifying config.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -f /app/config.json ]; then
+if [ ! -f /app/data/config.json ]; then
     echo "Creating config.json from template..."
-    cp /app/config.json.EXAMPLE /app/config.json
+    cp /app/config.json.EXAMPLE /app/data/config.json
 else
     echo "Updating config.json with any new fields from template..."
     tmp=$(mktemp)
-    jq -s '.[0] * .[1]' /app/config.json.EXAMPLE /app/config.json > "$tmp" && mv "$tmp" /app/config.json
+    jq -s '.[0] * .[1]' /app/config.json.EXAMPLE /app/data/config.json > "$tmp" && mv "$tmp" /app/data/config.json
 fi
 
 exec "$@"
