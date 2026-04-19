3f5402de001e582cdb7edbeea7ed6f0a9d961014
TymurD <tymur2910@gmail.com>
2026-04-19 18:42:47 +0300

    fix permissions errors

--- dockerfile
+++ dockerfile
@@ -2,12 +2,14 @@ FROM python:3.13.12-slim
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y jq && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y jq gosu && rm -rf /var/lib/apt/lists/*
 COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+
+USER root
 
 RUN chmod +x /app/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+USER_ID=${PUID:-1000}
+GROUP_ID=${PGID:-1000}
+
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+
 if [ ! -f /app/data/config.json ]; then
     echo "Creating config.json from template..."
     cp /app/config.json.EXAMPLE /app/data/config.json
@@ -9,4 +14,8 @@ else
     jq -s '.[0] * .[1]' /app/config.json.EXAMPLE /app/data/config.json > "$tmp" && mv "$tmp" /app/data/config.json
 fi
 
-exec "$@"
+chown -R $USER_ID:$GROUP_ID /app/data
+
+umask 002
+
+exec gosu $USER_ID:$GROUP_ID "$@"
